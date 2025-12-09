c794f2f16b79e3235ac28ecd55abd0e1ad006c7c
MatlabUser <myemail@example.com>
2025-12-09 11:49:15 +0800

    xh基本代码可跑通

--- 厚度影响/testMain.m
+++ 厚度影响/testMain.m
@@ -1,4 +1,4 @@
- clc
+clc
 clear
 %
 
@@ -21,63 +21,63 @@ d_radome = 0.06;
 [visibility_radome, visibility, TB_inv_radome, TB_inv, amp_ratio, delta_theta] = GetResult(SimuParam,d_radome);
 
 % 厚度影响
-% d = 0:0.005:0.1;
+d = 0:0.005:0.1;
 % 
-% amplitude_ratio = zeros(1,length(d));
-% deviation_theta = zeros(1,length(d));
-% parfor kk = 1 : length(d)
-%     [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult(SimuParam,d(kk))
+amplitude_ratio = zeros(1,length(d));
+deviation_theta = zeros(1,length(d));
+parfor kk = 1 : length(d)
+     [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult(SimuParam,d(kk))
+     amplitude_ratio(kk) = amp_ratio;
+     deviation_theta(kk) = delta_theta;
+end
+
+save('amplitude_ratio.mat','amplitude_ratio');
+save('deviation_theta.mat','deviation_theta');
+
+% load('result\厚度变化结果\边缘251,150\amplitude_ratio.mat');
+% load('result\厚度变化结果\边缘251,150\deviation_theta.mat');
+% load('amplitude_ratio.mat');
+% load('deviation_theta.mat');
+
+figure
+plot(d,amplitude_ratio.*100,'LineWidth',2);
+grid on
+title('加罩前后点源成像幅值比/%');
+
+figure
+plot(d,deviation_theta,'LineWidth',2);
+grid on
+title('加罩后点源成像偏离角/度');
+
+% % 介电常数影响
+% epsilon_relative = 2:0.1:5;
+% amplitude_ratio = zeros(1,length(epsilon_relative));
+% deviation_theta = zeros(1,length(epsilon_relative));
+% parfor kk = 1 : length(epsilon_relative)
+%     [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult(epsilon_relative(kk))
 %     amplitude_ratio(kk) = amp_ratio;
 %     deviation_theta(kk) = delta_theta;
 % end
 % 
 % save('amplitude_ratio.mat','amplitude_ratio');
 % save('deviation_theta.mat','deviation_theta');
-% 
-% % load('result\厚度变化结果\边缘251,150\amplitude_ratio.mat');
-% % load('result\厚度变化结果\边缘251,150\deviation_theta.mat');
 % % load('amplitude_ratio.mat');
 % % load('deviation_theta.mat');
 % 
-% figure
-% plot(d,amplitude_ratio.*100,'LineWidth',2);
-% grid on
-% title('加罩前后点源成像幅值比/%');
 % 
 % figure
-% plot(d,deviation_theta,'LineWidth',2);
+% plot(epsilon_relative,amplitude_ratio.*100,'LineWidth',2);
 % grid on
-% title('加罩后点源成像偏离角/度');
-
-% 介电常数影响
-epsilon_relative = 2:0.1:5;
-amplitude_ratio = zeros(1,length(epsilon_relative));
-deviation_theta = zeros(1,length(epsilon_relative));
-parfor kk = 1 : length(epsilon_relative)
-    [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult2(epsilon_relative(kk))
-    amplitude_ratio(kk) = amp_ratio;
-    deviation_theta(kk) = delta_theta;
-end
-
-save('amplitude_ratio.mat','amplitude_ratio');
-save('deviation_theta.mat','deviation_theta');
-% load('amplitude_ratio.mat');
-% load('deviation_theta.mat');
-
-
-figure
-plot(epsilon_relative,amplitude_ratio.*100,'LineWidth',2);
-grid on
-xlabel('天线罩介质相对介电常数'); 
-ylabel('加罩前后点源成像幅值比/%');
-title('加罩前后点源成像幅值比随天线罩介质相对介电常数变化关系');
-
-figure
-plot(epsilon_relative,deviation_theta,'LineWidth',2);
-grid on
-xlabel('天线罩介质相对介电常数'); 
-ylabel('加罩后点源成像偏离角/度');
-title('加罩后点源成像偏离角随天线罩介质相对介电常数变化关系');
+% xlabel('天线罩介质相对介电常数'); 
+% ylabel('加罩前后点源成像幅值比/%');
+% title('加罩前后点源成像幅值比随天线罩介质相对介电常数变化关系');
+% 
+% figure
+% plot(epsilon_relative,deviation_theta,'LineWidth',2);
+% grid on
+% xlabel('天线罩介质相对介电常数'); 
+% ylabel('加罩后点源成像偏离角/度');
+% title('加罩后点源成像偏离角随天线罩介质相对介电常数变化关系');
 
 
 toc
--- 成像及校正/testMain.m
+++ 成像及校正/testMain.m
@@ -17,8 +17,8 @@ SetParam();                   % 设置仿真参数及参数预处理
 addpath(genpath(pwd))
 SetParamTwo();
 
-d_radome = 0.06;
-[visibility_radome, visibility, TB_inv_radome, TB_inv, amp_ratio, delta_theta] = GetResult(SimuParam,d_radome);
+% d_radome = 0.06;
+% [visibility_radome, visibility, TB_inv_radome, TB_inv, amp_ratio, delta_theta] = GetResult(SimuParam,epsilon_r);
 
 % 厚度影响
 % d = 0:0.005:0.1;
@@ -49,35 +49,63 @@ d_radome = 0.06;
 % grid on
 % title('加罩后点源成像偏离角/度');
 
-% 介电常数影响
-epsilon_relative = 2:0.1:5;
-amplitude_ratio = zeros(1,length(epsilon_relative));
-deviation_theta = zeros(1,length(epsilon_relative));
-parfor kk = 1 : length(epsilon_relative)
-    [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult2(epsilon_relative(kk))
+% % 介电常数影响
+% epsilon_relative = 2:0.1:5;
+% amplitude_ratio = zeros(1,length(epsilon_relative));
+% deviation_theta = zeros(1,length(epsilon_relative));
+% parfor kk = 1 : length(epsilon_relative)
+%     [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult(SimuParam,epsilon_relative(kk))
+%     amplitude_ratio(kk) = amp_ratio;
+%     deviation_theta(kk) = delta_theta;
+% end
+% 
+% save('amplitude_ratio.mat','amplitude_ratio');
+% save('deviation_theta.mat','deviation_theta');
+% % load('amplitude_ratio.mat');
+% % load('deviation_theta.mat');
+% 
+% 
+% figure
+% plot(epsilon_relative,amplitude_ratio.*100,'LineWidth',2);
+% grid on
+% xlabel('天线罩介质相对介电常数'); 
+% ylabel('加罩前后点源成像幅值比/%');
+% title('加罩前后点源成像幅值比随天线罩介质相对介电常数变化关系');
+% 
+% figure
+% plot(epsilon_relative,deviation_theta,'LineWidth',2);
+% grid on
+% xlabel('天线罩介质相对介电常数'); 
+% ylabel('加罩后点源成像偏离角/度');
+% title('加罩后点源成像偏离角随天线罩介质相对介电常数变化关系');
+
+
+% 损耗角正切影响
+tangent_delta = 0:0.001:0.03;
+amplitude_ratio = zeros(1,length(tangent_delta));
+deviation_theta = zeros(1,length(tangent_delta));
+parfor kk = 1 : length(tangent_delta)
+    [~, ~, ~, ~,  amp_ratio, delta_theta ] = GetResult(SimuParam,tangent_delta(kk));
     amplitude_ratio(kk) = amp_ratio;
     deviation_theta(kk) = delta_theta;
 end
 
 save('amplitude_ratio.mat','amplitude_ratio');
 save('deviation_theta.mat','deviation_theta');
-% load('amplitude_ratio.mat');
-% load('deviation_theta.mat');
-
 
 figure
-plot(epsilon_relative,amplitude_ratio.*100,'LineWidth',2);
+plot(tangent_delta,amplitude_ratio.*100,'LineWidth',2);
 grid on
-xlabel('天线罩介质相对介电常数'); 
+xlabel('天线罩介质损耗正切'); 
 ylabel('加罩前后点源成像幅值比/%');
-title('加罩前后点源成像幅值比随天线罩介质相对介电常数变化关系');
+title('加罩前后点源成像幅值比随天线罩介质损耗正切变化关系');
+
 
 figure
-plot(epsilon_relative,deviation_theta,'LineWidth',2);
+plot(tangent_delta,deviation_theta,'LineWidth',2);
 grid on
-xlabel('天线罩介质相对介电常数'); 
+xlabel('天线罩介质损耗正切'); 
 ylabel('加罩后点源成像偏离角/度');
-title('加罩后点源成像偏离角随天线罩介质相对介电常数变化关系');
-
+title('加罩后点源成像偏离角随天线罩介质损耗正切变化关系');
 
 toc
